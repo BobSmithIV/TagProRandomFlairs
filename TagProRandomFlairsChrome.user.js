2535a1ad09faa2b8f5385c6270deb9cfd3832074
BobSmithIV <BobSmithIV@users.noreply.github.com>
2015-03-21 01:22:27 +0000

    Added user interface to create rotation

--- TagProRandomFlairsChrome.user.js
+++ TagProRandomFlairsChrome.user.js
@@ -4,61 +4,35 @@
 // @description   Randomly choose a new flair each game
 // @include       http://tagpro-*.koalabeast.com*
 // @author        BobSmithIV, with some code inspired by ballparts' extensions
-// @version       1.1
+// @version       2.0
 // @grant         GM_getValue
 // @grant         GM_setValue
 // @downloadURL   https://raw.githubusercontent.com/BobSmithIV/TagProRandomFlairs/master/TagProRandomFlairsChrome.user.js
 // ==/UserScript==
 
-var flairsToInclude = [	//Flairs to include.  Change the 'true' to 'false' to remove that flair from the flair rotation.  
-true,	// TagPro Developer
-true,	// Communtiy Contributor
-true,	// Level 1 Donor ($10)
-true,	// Level 2 Donor ($40)
-true,	// Level 3 Donor ($100)
-true,	// Community Contest Winner
-true,	// Monthly Leader Board Winner
-true,	// Weekly Leader Board Winner
-true,	// Daily Leader Board Winner
-true,	// Happy Birthday TagPro
-true,	// Lucky You
-true,	// How Foolish
-true,	// Hare Today, Goon Tomorrow
-true,	// Unfortunate Sniper Hacks TagPro
-true,	// So Very Scary
-true,	// Daryl Would Be Proud
-true,   // Happy 2nd Birthday TagPro
-true,   // Tower 1-1 Complete
-true,   // Good and Lucky
-true,	// Bacon (6°)
-true,	// Moon (11°)
-true,	// Freezing (32°)
-true,	// Dolphin (42°)
-true,	// Alien (51°)
-true,	// Road Sign (66°)
-true,	// Peace (69°)
-true,	// Flux Capacitor (88°)
-true,	// Microphone (98°)
-true,	// Boiling (100°)
-true,	// Dalmations (101°)
-true,	// ABC (123°)
-true,	// Love (143°)
-true,	// Pokemon (151°)
-true,	// Phi (162°)
-true,	// U Turn (180°)
-true,	// World (196°)
-true,	// Bowling (300°)
-true	// Pi (314°)
-];
-
-
-// ***  Don't change anything beneath this!  ***
-
-//create the flair rotation
+//the name TagPro gives to each of the flairs
 var flairNames = ['special.developer','special.helper','special.supporter','special.supporter2','special.supporter3','special.contest','boards.month','boards.week','boards.day',
                   'event.birthday','event.stPatricksDay','event.aprilFoolsDay','event.easter','event.hacked','event.halloween','event.survivor', 'event.birthday2', 'event.platformer', 'event.stPatricksDay2',
                   'degree.bacon','degree.moon','degree.freezing','degree.dolphin','degree.alien','degree.roadsign','degree.peace','degree.flux','degree.microphone','degree.boiling',
                   'degree.dalmations','degree.abc','degree.love','degree.pokemon','degree.phi','degree.uturn','degree.world','degree.bowling','degree.pi'];
+
+//intialize variables the first time the script is run
+if(!GM_getValue('savedFlairRotation')){
+    GM_setValue('randomizeState','unrandomized');
+    GM_setValue('savedFlairRotation',new Array(flairNames.length + 1).join('1'));
+}
+
+//fill the array from the saved string (silly TamperMonkey can't store arrays)
+var flairsToInclude = [];
+for (var i = 0; i<flairNames.length;i++){
+    if (GM_getValue('savedFlairRotation').charAt(i)=='1'){
+        flairsToInclude[i]=true;
+    }else{
+        flairsToInclude[i]=false;
+    }
+}
+
+//create the flair rotation
 var flairRotation = [];
 for (var i = 0; i<flairsToInclude.length; i++){
     if (flairsToInclude[i]){
@@ -66,10 +40,6 @@ for (var i = 0; i<flairsToInclude.length; i++){
     }
 }
 
-//intialize variables the first time the script is run
-if(!GM_getValue('randomizeState')){
-    GM_setValue('randomizeState','unrandomized');
-}
 
 //work out the user's current server:
 GM_setValue('server', window.location.href.substring(window.location.href.indexOf('tagpro-')+7, window.location.href.indexOf('.koalabeast.com')));
@@ -104,36 +74,94 @@ if(document.URL.match(/groups\/./) && document.URL.search('create')<0 && GM_getV
 
 
 //if you've been sent to the profile to randomize flairs, pick any available flair at random, then go back to where you came from
-if( document.URL.search('profile') >= 0 && (GM_getValue('randomizeState').search('sentToRandomize')>=0)) {
-
-    //randomly select a flair to use
-    elements = document.getElementsByTagName('input');
-    flairs = [];
-    for(var i=0; i<elements.length; i++) {
-        //if the object found is indeed a flair radio, and that flair is in the current flair rotation, add that to the chooseable options
-        if(elements[i].name=="selectedFlair" && flairRotation.indexOf(elements[i].value)>=0){
-            flairs.push(elements[i]);
+if( document.URL.search('profile') >= 0 ){
+    //create the new column's header:
+    x=getBoard().firstChild.childNodes;
+    var newCell = x[0].appendChild(document.createElement('th'));
+    newCell.innerHTML = 'Rotation';
+    
+    for (var i = 1; i<x.length;i++){
+        //create the new cell
+        newCell = x[i].appendChild(document.createElement('td'));
+        
+        //if you've earned the flair,
+        if ((x[i].className!='fade')){
+            //check that the flair is currently supported
+            indexOfFlair = flairNames.indexOf(x[i].childNodes[3].firstChild.value);
+            if (indexOfFlair<0){
+                newCell.innerHTML="Flair not supported - check github.com/BobSmithIV, then message Bob at reddit.com/u/Bob_Smith_IV if it's yet to be updated";
+            }else{
+                //add a checkbox to the cell
+                checkbox = newCell.appendChild(document.createElement('input'));
+                checkbox.type = "checkbox";
+                checkbox.id = indexOfFlair;
+                
+                if (flairsToInclude[indexOfFlair]){
+                    checkbox.checked=true;
+                }
+                
+                //toggle this flair in/out of rotation when the checkbox is clicked
+                checkbox.onclick = function() {
+                    if (this.checked){
+                        flairsToInclude[this.id]=true;
+                    }else{
+                        flairsToInclude[this.id]=false;
+                    }
+                    stringified='';
+                    for (var j=0; j<flairsToInclude.length;j++){
+                        stringified= stringified+(flairsToInclude[j]?'1':'0');
+                    }
+                    GM_setValue('savedFlairRotation',stringified);
+                };
+            }
         }
     }
-    chosenFlair = flairs[Math.floor(Math.random()*flairs.length)];
-    chosenFlair.click();
     
-    //if sent from group, set to return to group, else set to return to joiner
-    if (GM_getValue('randomizeState')=='sentToRandomizeFromGroup'){
-        returnTo='http://tagpro-'+GM_getValue('server')+'.koalabeast.com/groups/'+GM_getValue('groupName');
-    }else{
-        returnTo = 'http://tagpro-'+GM_getValue('server')+'.koalabeast.com/games/find/';
+    if (GM_getValue('randomizeState').search('sentToRandomize')>=0){
+        
+        
+        //randomly select a flair to use
+        elements = document.getElementsByTagName('input');
+        flairs = [];
+        for(var i=0; i<elements.length; i++) {
+            //if the object found is indeed a flair radio, and that flair is in the current flair rotation, add that to the chooseable options
+            if(elements[i].name=="selectedFlair" && flairRotation.indexOf(elements[i].value)>=0){
+                flairs.push(elements[i]);
+            }
+        }
+        
+        //if the user has at least one flair in the rotation, click one at random
+        if (flairs.length>0){
+            chosenFlair = flairs[Math.floor(Math.random()*flairs.length)];
+            chosenFlair.click();
+        }
+        //if sent from group, set to return to group, else set to return to joiner
+        if (GM_getValue('randomizeState')=='sentToRandomizeFromGroup'){
+            returnTo='http://tagpro-'+GM_getValue('server')+'.koalabeast.com/groups/'+GM_getValue('groupName');
+        }else{
+            returnTo = 'http://tagpro-'+GM_getValue('server')+'.koalabeast.com/games/find/';
+        }
+
+        //save that we've now randomized the flair
+        GM_setValue('randomizeState','randomized');
+
+        //return to whence you came
+        window.location.href = returnTo;
     }
-    
-    //save that we've now randomized the flair
-    GM_setValue('randomizeState','randomized');
-    
-    //return to whence you came
-    window.location.href = returnTo;
 }
 
 
 //if a game starts, not from a group, get ready to randomize the flair afterwards
 if( document.URL.search(':80') >= 0){
 	GM_setValue('randomizeState','unrandomized');
+}
+
+//get the leaderboard from the dom
+function getBoard(){
+    var x = document.getElementsByClassName("board");
+    for (var i=x.length-1;i>=0;i--){
+        if (x[i].childNodes[0].childNodes[0].childNodes[1].innerText=="Award"){
+            return x[i];
+        }
+    }
 }
