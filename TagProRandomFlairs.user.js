9ad7555e4594564ed0d93cb05b5e3876d1da4e9d
BobSmithIV <bobsmith0030+git@gmail.com>
2015-08-04 00:27:58 +0100

    Fitted favicon flair functionality
    Plus minor prettification of the table and the code

--- TagProRandomFlairs.user.js
+++ TagProRandomFlairs.user.js
@@ -12,6 +12,15 @@
 
 // Note that unlike in version 1.X of this script, to change which flairs to include in the flair rotation, visit your profile and tick the checkboxes of those flairs you want included. 
 
+// initialize the favicon toggle variable the first time the script is run
+if (GM_getValue('toggleFavicon')===undefined ){
+    GM_setValue('toggleFavicon',false);
+
+// change the favicon to the current flair if the user has selected that functionality
+} else if (GM_getValue('toggleFavicon')){
+    changeFavicon(GM_getValue('currentFlair'));
+}
+
 // load the saved JSON if it exists
 var flairRotation = 'needsBuilt';
 if (GM_getValue('flairs')){
@@ -42,9 +51,36 @@ if(document.URL.search('games/find')>=0 || document.URL.search('group')>=0) {
     pickNewFlair();
 }
 
-//if you're on the profile, add the new column to allow users to select the flair rotation
+//if you're on the profile, add the new UI to allow users to select the flair rotation
 if( document.URL.search('profile') >= 0 ){
-    
+	
+    // make the 'toggle favicon' row and checkbox
+	var row = getBoard().lastChild.appendChild(document.createElement('tr'));
+	row.style.marginBottom='10px';
+	row.style.height='50px';
+	row.appendChild(document.createElement('td'));
+	var label = row.appendChild(document.createElement('td'));
+	row.appendChild(document.createElement('td'));
+	row.appendChild(document.createElement('td'));
+	var checkboxContainer = row.appendChild(document.createElement('td'));
+	label.innerHTML='Change favicon to current flair'
+	var faviconCheckbox = checkboxContainer.appendChild(document.createElement('input'));
+	faviconCheckbox.type = 'checkbox';
+	faviconCheckbox.id = 'faviconCheckbox';
+    // set it to checked if appropriate
+	if (GM_getValue('toggleFavicon')===true){
+		faviconCheckbox.checked=true;
+	}
+    // toggle changing the favicon when this is clicked
+	faviconCheckbox.onclick = function() {
+		GM_setValue('toggleFavicon',this.checked);
+        // if not checked, forget the saved flair
+        if (!this.checked){
+            GM_setValue('currentFlair','initial');
+            changeFavicon('initial');
+        }
+	}
+	
     //create the new column's header:
     x=getBoard().firstChild.childNodes;
     var newCell = x[0].appendChild(document.createElement('th'));
@@ -53,9 +89,12 @@ if( document.URL.search('profile') >= 0 ){
     //initialize the list of all flairs
     allFlairs = [];
     
-    for (var i = 1; i<x.length;i++){
+    for (var i = 1; i<x.length-1;i++){
         //create the new cell
         newCell = x[i].appendChild(document.createElement('td'));
+		if (i%2===0){
+			newCell.className = 'alt';
+		}
         
         //if you've earned the flair,
         if (x[i].className!='fade'){
@@ -81,11 +120,7 @@ if( document.URL.search('profile') >= 0 ){
 
             //toggle this flair in/out of rotation when the checkbox is clicked
             checkbox.onclick = function() {
-                if (this.checked){
-                    flairRotation[this.id]=true;
-                }else{
-                    flairRotation[this.id]=false;
-                }
+                flairRotation[this.id]=this.checked;
                 GM_setValue('flairs',JSON.stringify(flairRotation));
             }
         }
@@ -103,6 +138,22 @@ if( document.URL.search('profile') >= 0 ){
             window.location.href = 'http://tagpro-'+GM_getValue('server')+'.koalabeast.com/'
         }
     }
+    
+    // change favicon if the user changes their flair manually too
+    $('body').click(function(){
+        if (GM_getValue('toggleFavicon')){
+            elements = document.getElementsByTagName('input'); 
+            for(var i=0; i<elements.length; i++) {
+                if(elements[i].name=="selectedFlair" && elements[i].checked){ // if this is a selected flair radio,
+                    if (GM_getValue('currentFlair')!=elements[i].value){ // if the selected flair is different to what it was before
+                        GM_setValue('currentFlair',elements[i].value); // save the new value
+                        changeFavicon(elements[i].value); // change the favicon 
+                    }
+                }
+            }
+        }
+    });
+    
 }
 
 function pickNewFlair(){
@@ -131,6 +182,9 @@ function pickNewFlair(){
     }
     // send the POST request
     xmlhttp.send('flair='+chosenFlair);
+    if (GM_getData('toggleFavicon')){
+        changeFavicon(chosenFlair);
+    }
 }
 
 //get the leaderboard from the dom
@@ -141,4 +195,17 @@ function getBoard(){
             return x[i];
         }
     }
+}
+
+// changes the favicon to the inputted flair image, as hosted by myself on GitHub
+function changeFavicon(flair) {
+    var link = document.createElement('link');
+    var oldLink = document.getElementById('dynamic-favicon');
+    link.id = 'dynamic-favicon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://raw.githubusercontent.com/BobSmithIV/TagProFaviconFlair/master/flairs/'+flair+'.png';
+    if (oldLink) {
+        document.head.removeChild(oldLink);
+    }
+    document.head.appendChild(link);
 }
